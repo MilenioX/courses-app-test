5eaad470a82cb98c8c8527fe01a3cbe05652644a
Milenio X <jnkmiloq@gmail.com>
2023-01-17 09:37:30 -0500

    Added SLF4J tool as logger implementation

--- src/main/java/com/mundox/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/mundox/courseinfo/cli/CourseRetriever.java
@@ -1,22 +1,27 @@
 package com.mundox.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        System.out.println("CourseRetriever started!");
+        LOG.info("CourseRetriever starting");
         if (args.length == 0 ) {
-            System.out.println("Please provide an author name as first argument");
+            LOG.warn("Please provide an author name as first argument");
             return ;
         }
         try {
             retrieveCourses(args[0]);
         } catch (Exception e) {
-            System.out.println("Unexpected error");
+            LOG.error("Unexpected error");
             e.printStackTrace();
         }
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for author: " + authorId);
+        LOG.info("Retrieving courses for author '{}'", authorId);
     }
 }
