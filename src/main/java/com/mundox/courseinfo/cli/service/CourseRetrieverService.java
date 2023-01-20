d9e87d4dc0d25bc770dd76ff54c46b31fcd463ad
Milenio X <jnkmiloq@gmail.com>
2023-01-19 23:43:11 -0500

    Improving HTTP Client and status code validation

--- src/main/java/com/mundox/courseinfo/cli/service/CourseRetrieverService.java
+++ src/main/java/com/mundox/courseinfo/cli/service/CourseRetrieverService.java
@@ -10,7 +10,10 @@ public class CourseRetrieverService {
 
     private static final String PS_URI = "https://app.pluralsight.com/profile/data/author/%s/all-content";
 
-    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final HttpClient CLIENT = HttpClient
+            .newBuilder()
+            .followRedirects(HttpClient.Redirect.ALWAYS)
+            .build();
 
     public String getCoursesFor(String authorID) {
         HttpRequest request = HttpRequest
@@ -19,7 +22,11 @@ public class CourseRetrieverService {
                 .build();
         try {
             HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return switch(response.statusCode()) {
+                case 200 -> response.body();
+                case 404 -> "";
+                default -> throw new RuntimeException("Pluralsight API call failed with status code " + response.statusCode());
+            };
         } catch (IOException | InterruptedException ex) {
             throw new RuntimeException("Could not call Pluralsight API", ex);
         }
