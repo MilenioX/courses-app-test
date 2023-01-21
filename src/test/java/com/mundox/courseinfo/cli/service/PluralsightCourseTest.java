2bf62eb9d902e040d8d7edf4ddf3c5524912b4bc
Milenio X <jnkmiloq@gmail.com>
2023-01-21 12:16:58 -0500

    Improved unit test to use parameterized test and added the maven plugin to view test in the mvn console

--- src/test/java/com/mundox/courseinfo/cli/service/PluralsightCourseTest.java
+++ src/test/java/com/mundox/courseinfo/cli/service/PluralsightCourseTest.java
@@ -1,24 +1,21 @@
 package com.mundox.courseinfo.cli.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PluralsightCourseTest {
 
-    @Test
-    void durationInMinutes() {
-        PluralsightCourse course = new PluralsightCourse("id", "Test course", "00:05:37", "url", false);
-        assertEquals(5, course.durationInMinutes());
-    }
-
-    void durationInMinutesOverHour() {
-        PluralsightCourse course = new PluralsightCourse("id", "Test course", "01:08:54.9613330", "url", false);
-        assertEquals(68, course.durationInMinutes());
-    }
-
-    void durationInMinutesZero() {
-        PluralsightCourse course = new PluralsightCourse("id", "Test course", "00:00:00", "url", false);
-        assertEquals(68, course.durationInMinutes());
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            01:08:54.9613330, 68
+            00:05:37, 5
+            00:00:00.0, 0
+            """)
+    void durationInMinutes(String input, int expected) {
+        PluralsightCourse course = new PluralsightCourse("id", "Test course", input, "url", false);
+        assertEquals(expected, course.durationInMinutes());
     }
 }
